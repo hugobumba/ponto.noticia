cd45fb72c02d60970aae0636a7e4bb1439290270
Hugo <40358@alunos.upt.pt>
2024-09-30 03:27:17 +0100

    First commit of the apps interface

---
+++ src/content/config.ts
@@ -0,0 +1,34 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from 'astro:content';
+
+// 2. Define your collection(s)
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    desc: z.string(),
+    img: z.string(),
+    sourceName: z.string(),
+    sourceUrl: z.string(),
+    link: z.string()
+  }),
+});
+
+const teamCollection = defineCollection({
+  schema: z.object({
+    draft: z.boolean(),
+    name: z.string(),
+    title: z.string(),
+    avatar: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+    publishDate: z.string().transform(str => new Date(str)),
+  }),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  'blog': blogCollection,
+  'team': teamCollection,
+};
