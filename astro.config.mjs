0d38a5ac23d23b9648915dbde3f04824ea496ce3
Hugo <40358@alunos.upt.pt>
2025-03-08 02:20:57 +0000

    API consuming adjustmens

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
+  //output: 'static',
+  base: "/",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
